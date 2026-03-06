b0da80288c4ca4f9e65231b23c8fc2f4a55b8e47
yasoaresss <yasguedes@outlook.com>
2026-03-05 21:51:15 -0300

    add /

--- testes-playwright/playwright.config.ts
+++ testes-playwright/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'https://yasoaresss.github.io/02-TesteAutomatizado',
+    baseURL: 'https://yasoaresss.github.io/02-TesteAutomatizado/',
     trace: 'on-first-retry',
   },
 
